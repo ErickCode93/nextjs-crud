9fd41614ed072b25271e191a52632a2861aa0cd8
ErickCode93 <erickalvacontact@gmail.com>
2025-05-21 03:28:51 -0600

    comment values no never used

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,28 +4,16 @@ export async function GET() {
   return NextResponse.json("");
 }
 
-export async function POST(request: Request) {
+export async function POST() {
   return NextResponse.json({
     message: "User created successfully",
   });
 }
 
-export async function PUT(request: Request) {
-  // const data = await request.json();
-  // const { index, user } = data; // expects { index: number, user: { name, email } }
-  // if (typeof index === "number" && users[index]) {
-  //   users[index] = user;
-  //   return NextResponse.json({ message: "User updated", user });
-  // }
+export async function PUT() {
   return NextResponse.json({ message: "User not found" }, { status: 404 });
 }
 
-export async function DELETE(request: Request) {
-  // const data = await request.json();
-  // const { index } = data; // expects { index: number }
-  // if (typeof index === "number" && users[index]) {
-  //   const deleted = users.splice(index, 1);
-  //   return NextResponse.json({ message: "User deleted", user: deleted[0] });
-  // }
+export async function DELETE() {
   return NextResponse.json({ message: "User not found" }, { status: 404 });
 }
